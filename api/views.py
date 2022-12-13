0e92ba38e60d26a3c1ac331290230cb7618a2951
nazif-malhi <nazifmalhi@gmail.com>
2022-12-13 23:35:25 +0500

    checking tensorflow

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+import tensorflow as tf
 
 
 def page(request):
     print('runing yes')
-    return HttpResponse("Hello World")
+    print(tf.__version__)
+    return HttpResponse("Hello World tf ---> version" + tf.__version__)
